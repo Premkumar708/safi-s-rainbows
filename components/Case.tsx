f69d55c2a325efa6d8d23e38391e7321cde970f1
Kailash1233 <kailash61203@gmail.com>
2024-12-02 21:29:49 +0530

    image links added

--- components/Case.tsx
+++ components/Case.tsx
@@ -77,3 +77,27 @@ export const Case = () => {
     </div>
   );
 };
+
+// Blouses
+// https://5.imimg.com/data5/SELLER/Default/2023/6/312586787/HX/YB/RP/150875762/cotton-boutique-desgin-blouses-500x500.jpg
+
+// Salwar
+// https://imgmedia.lbb.in/media/2019/03/5c9b4dc9feedea3720644a44_1553681865377.jpg
+
+// Sarees
+// https://imgmedia.lbb.in/media/2019/08/5d5167ebe24af71f9588db98_1565616107200.jpg
+
+// Inskirts
+// https://5.imimg.com/data5/FN/OI/AI/SELLER-37408908/amala-500x500.JPG
+
+// Bindis
+// https://images-cdn.ubuy.co.id/6351a15f95845a24687fb0c3-6-packs-42-bindis-mix-n-match-face.jpg
+
+// Casual Wears
+// https://s.alicdn.com/@sc04/kf/H5d7b335005834deda48ebf04f4f15528a.jpg
+
+// Wedding Wear
+// https://imgmedia.lbb.in/media/2019/07/5d237968e93a896e55427d91_1562605928333.jpg
+
+// Silk
+// https://content3.jdmagicbox.com/v2/comp/chennai/70/044p6001170/catalogue/silkworm-boutique-thousand-lights-chennai-boutiques-2v3270l.jpg
